8e04b5825d42936e08e39b98b6534cae5428416a
Madhurima <84146595+madhurima5978@users.noreply.github.com>
2025-03-16 19:05:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/"
+  base: './',  // Ensures correct asset paths for S3 deployment
   build: {
-    outDir: 'dist',  // Ensure this matches your CodePipeline buildspec
+    outDir: 'dist'
   }
-})
+});
